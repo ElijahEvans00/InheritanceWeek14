964153f6276249e2ae4a5833542cf740d76e3cc2
Elijah Evans <elijah.evans1@sinclair.edu>
2024-04-19 22:36:33 -0400

    Created the Truck header and .cpp with all the necessary components.

--- InheritancePoly/Truck.cpp
+++ InheritancePoly/Truck.cpp
@@ -1,3 +1,23 @@
 // Elijah Evans
 // CIS 1202 201
 // April 19, 2024
+#include "Truck.h"
+#include <iostream>
+
+using namespace std;
+
+Truck::Truck(string manufacturer, int yearBuilt, int towingCapacity)
+    : Vehicle(manufacturer, yearBuilt), towingCapacity(towingCapacity) {}// Constructor
+
+int Truck::getTowingCapacity() const {
+    return towingCapacity; // Returns towingCapacity
+}
+
+void Truck::setTowingCapacity(int towingCapacity) {
+    this->towingCapacity = towingCapacity; // Sets towingCapacity
+}
+
+void Truck::displayInfo() const {
+    Vehicle::displayInfo(); // Calls displayInfo from Vehicle.cpp
+    cout << "Towing Capacity: " << towingCapacity << endl; // Displays towingCapacity
+}
--- InheritancePoly/Truck.h
+++ InheritancePoly/Truck.h
@@ -2,3 +2,19 @@
 // CIS 1202 201
 // April 19, 2024
 #pragma once
+#ifndef TRUCK_H
+#define TRUCK_H
+
+#include "Vehicle.h"
+// Defines Truck class as child of Vehicle
+class Truck : public Vehicle {
+private:
+    int towingCapacity;
+public:
+    Truck(std::string manufacturer, int yearBuilt, int towingCapacity); // Constructor
+    int getTowingCapacity() const;
+    void setTowingCapacity(int towingCapacity);
+    void displayInfo() const;
+};
+
+#endif
